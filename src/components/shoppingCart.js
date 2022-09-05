0dd34f22bef428198dcd4fe2177c0543bf0e1f0d
Aris Empanta <eams220891@gmail.com>
2022-09-05 05:24:17 +0300

    Working on shopping cart unique selected items

--- src/components/shoppingCart.js
+++ src/components/shoppingCart.js
@@ -2,32 +2,37 @@ import "../css/shoppingCart.css"
 import { socket } from "./chat"
 import { useSelector, useDispatch } from "react-redux"
 import { clearCart } from "../redux/slice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const ShoppingCart = () => {
-
+    
+    const [localCart, setLocalCart ] = useState([])
     
     //The apis to access and edit the cart state from redux toolkit store
     const cart = useSelector((state) => state.cart.value)    
     const dispatch = useDispatch()
 
     useEffect(() => {
-        console.log(cart)
-    })
+      
+      // With the Map object and the array.map method, we keep only unique id values from the cart state
+      const uniqueItems = [...new Map( cart.map(item => [item.id, item] )).values()]
+
+      setLocalCart(uniqueItems)
+    }, [])
 
     const sendOrder = () => {
 
-        socket.emit('send order', 'pizza')
+        socket.emit('send order', 'pizza')        
     }
 
     
 
     return(<div className="shoppingCart">
-                {cart.map(item => <div className="cartItem">
-                                      <img className="cartImage" src={item.image} />
-                                      <p>{ item.name }</p>
-                                      <p>Quantity: </p>
-                                  </div>)}
+                {localCart.map((item) => <div>
+                                            <p>{ item.name }</p>
+                                            <p>Quantity : { cart.filter( value => value.id === item.id).length} </p>
+                                         </div>                      )}
                 <button onClick={ sendOrder }>send order</button>
                 <button onClick={ () => dispatch(clearCart()) }>Clear cart</button>
            </div>)
