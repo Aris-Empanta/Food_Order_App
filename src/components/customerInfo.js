dccc9a8d5fd543cdfda6656e4175b7c301d28790
Aris Empanta <eams220891@gmail.com>
2022-10-16 04:13:43 +0300

    Working on customerInfo component

--- src/components/customerInfo.js
+++ src/components/customerInfo.js
@@ -22,7 +22,7 @@ export const CustomerInfo = () => {
     const cart = useSelector( state => state.customer.finalCart )
 
     useEffect(() => console.log(cart))
-      
+       
     // We send a verification code to the customer and 
     //store it in the redux store.
     const getPassword = (e) => {
@@ -85,34 +85,36 @@ export const CustomerInfo = () => {
 
     return(<div className='customerInfoSection'>
               <div className = "customerInfo" >
-                <h2>Delivery details</h2>
-                <form id='customerForm'>
-                  <label>Name/Doorbell</label>
+                <h2 className='formHeading'>Delivery details</h2>
+                <form id='customerForm'>                  
+                  <label id='nameLabel' className='labels'>Name</label>
                   <input type="text" id="enterName"/>
-                  <label>address</label>
-                  <input type="text" id="enterAddress" />
-                  <label>floor</label>
+                  <label id='addressLabel' className='labels'>Address</label>
+                  <input type="text" id="enterAddress" />                  
+                  <label id='floorLabel' className='labels'>Floor</label>
                   <input type="text" id="enterFloor" /> 
-                  <label>mobile phone</label>
-                  <input type="number" id="enterPhone" />
-                  <label>email</label>
+                  <label id='phoneLabel' className='labels'>Mobile phone</label>
+                  <input type="number" id="enterPhone" />                    
+                  <label id='emailLabel' className='labels'>Email</label>
                   <input type="email" id="enterEmail" />
-                  <label>Additional comments</label>
+                  <label id='commentsLabel' className='labels'>Additional comments</label>
                   <textarea type="text" id="enterComments" />
                   <div id="rememberMe">
                     <input type="checkbox" id="saveInfoPermission"/>
                     <p> Remember me </p>
                   </div>
-                  <button onClick={ getPassword }>submit</button>
+                  <button onClick={ getPassword } id="submitForPassword">submit</button>
                 </form> 
               </div>
-              <div>
-                <h2>Already a customer? Please just enter your email</h2>
-                <label>email</label><br />
-                <input type="email" id="knownMail"/><br />
-                <label>Additional comments</label><br />
-                <textarea type="text" id="knownComments" /><br />
-                <button onClick={ handleRegisteredCustomer }> Submit </button>
+              <div className = "registeredInfo">
+                <h2 className='formHeading'>Already a customer? Just enter your email</h2>
+                <form className='registeredForm'>
+                  <label className='labels'>Email</label><br />
+                  <input type="email" id="knownMail"/><br />
+                  <label className='labels'>Additional comments</label><br />
+                  <textarea type="text" id="knownComments" /><br />
+                  <button onClick={ handleRegisteredCustomer } id="submitRegistered"> Submit </button>
+                </form>
               </div>
             </div>
           )    
